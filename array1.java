33dabfa1b8ad40138beeac124869fc280ab4a44d
Sandip6369 <59789598+Sandip6369@users.noreply.github.com>
2021-06-14 17:48:39 +0530

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,22 @@
+package world;
+import java.util.*;
+import java.util.Scanner;
+
+public class array1 {
+		 public static void main(String args[])
+		 {
+		 	Scanner kb= new Scanner(System.in);
+		 	System.out.println("enter the no. of elements");
+		 	int n = kb.nextInt();
+		 	int arr[]=new int [n];
+		 	for(int i=0;i<n;i++)
+		 	{
+		 		arr[i]=kb.nextInt();
+		 	}
+		 	for(int i=0;i<n;i++)
+		 	{
+		 		System.out.println( arr[i]);
+		 		
+		 	}
+		 }
+		 }
